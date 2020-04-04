4447c4f5689de02af19ef60d6c601b77d3d84d53
Christian Bär <teggno@hotmail.com>
2020-04-04 20:58:56 +0200

    Updated dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,26 @@
 var webpackConfig = require("./webpack.development");
 
-module.exports = function(config) {
-    config.set({
-        basePath: "",
-        frameworks: ["mocha", "chai", "sinon"],
-        files: ["test/*.ts"],
-        exclude: [],
-        preprocessors: {
-            "test/**/*.ts": ["webpack"]
-        },
-        webpack: {
-            mode: webpackConfig.mode,
-            module: webpackConfig.module,
-            resolve: webpackConfig.resolve
-        },
-        reporters: ["junit", "mocha"],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ["PhantomJS"],
-        singleRun: false,
-        concurrency: Infinity
-    });
+module.exports = function (config) {
+  config.set({
+    basePath: "",
+    frameworks: ["mocha", "chai", "sinon"],
+    files: ["test/*.ts"],
+    exclude: [],
+    preprocessors: {
+      "test/**/*.ts": ["webpack"],
+    },
+    webpack: {
+      mode: webpackConfig.mode,
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve,
+    },
+    reporters: ["junit", "mocha"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["jsdom"],
+    singleRun: false,
+    concurrency: Infinity,
+  });
 };
